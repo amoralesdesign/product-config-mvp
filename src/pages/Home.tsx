78fe4daff5ac784048b196e587cc6bf393981a9f
amoralesdesign <amoralesdesign@gmail.com>
2025-06-04 12:46:55 +0200

    fix: deploy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { ItemSelector } from "../components"
 import { useItemStore } from "../store/useItemStore"
 import { useCategoryStore } from "../store/useCategoryStore"
