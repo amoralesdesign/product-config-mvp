12067a4682d953e64e6cd0ca10205fec28c0e99f
amoralesdesign <amoralesdesign@gmail.com>
2025-06-04 06:16:50 +0200

    feat: AppRouter config

---
+++ src/pages/Home.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+import { ItemSelector } from "../components"
+import { useItemStore } from "../store/useItemStore"
+import { useCategoryStore } from "../store/useCategoryStore"
+import { useRuleStore } from "../store/useRuleStore"
+import { Logo } from "../components"
+
+export const Home = () => {
+  const { items, fetchData } = useItemStore()
+  const { categories } = useCategoryStore()
+  const { fetchRules } = useRuleStore()
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    fetchData()
+    fetchRules()
+    setIsLoading(false)
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="absolute z-10 bg-zinc-900 top-0 bottom-0 left-0 right-0 text-white flex items-center justify-center">
+        <Logo />
+      </div>
+    )
+  }
+
+  return (
+    <>
+      {categories.map((cat) => (
+        <ItemSelector
+          key={cat.id}
+          id={cat.id}
+          title={cat.name}
+          order={cat.order}
+          items={items.filter(item => item.category === cat.id)}
+        />
+      ))}
+    </>
+  );
+};
