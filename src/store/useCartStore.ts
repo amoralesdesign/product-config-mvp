3c116bf60faea71ed812c582ee71b247b26e624c
amoralesdesign <amoralesdesign@gmail.com>
2025-06-05 23:48:30 +0200

    refactor: use persist with zustand

--- src/store/useCartStore.ts
+++ src/store/useCartStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 import { Item } from '../models/item.model'
 
 interface CartStore {
@@ -9,30 +10,29 @@ interface CartStore {
   isSelected: (item: Item) => boolean
 }
 
-const getInitialCart = (): Item[] => {
-  const stored = localStorage.getItem('cart')
-  return stored ? JSON.parse(stored) : []
-};
-
-export const useCartStore = create<CartStore>((set, get) => ({
-  cart: getInitialCart(),
-  addToCart: (item) => {
-    const cart = get().cart;
-    const filtered = cart.filter((i) => i.category !== item.category)
-    const newCart = [...filtered, item]
-    set({ cart: newCart })
-    localStorage.setItem('cart', JSON.stringify(newCart))
-  },
-  removeFromCart: (categoryId) => {
-    const cart = get().cart.filter((i) => i.category !== categoryId)
-    set({ cart })
-    localStorage.setItem('cart', JSON.stringify(cart))
-  },
-  clearCart: () => {
-    set({ cart: [] })
-    localStorage.removeItem('cart')
-  },
-  isSelected: (item) => {
-    return get().cart.some((i) => i.id === item.id)
-  },
-}))
+export const useCartStore = create<CartStore>()(
+  persist(
+    (set, get) => ({
+      cart: [],
+      addToCart: (item) => {
+        const cart = get().cart;
+        const filtered = cart.filter((i) => i.category !== item.category)
+        const newCart = [...filtered, item]
+        set({ cart: newCart })
+      },
+      removeFromCart: (categoryId) => {
+        const cart = get().cart.filter((i) => i.category !== categoryId)
+        set({ cart })
+      },
+      clearCart: () => {
+        set({ cart: [] })
+      },
+      isSelected: (item) => {
+        return get().cart.some((i) => i.id === item.id)
+      },
+    }),
+    {
+      name: 'cart-storage', // Clave en localStorage
+    }
+  )
+)
